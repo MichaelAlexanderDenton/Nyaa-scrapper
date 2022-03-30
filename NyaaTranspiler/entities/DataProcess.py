3a2c7efe923f9dbfe49c2060f1800d5db3820eec
Mike_Denton <lunaticberserker@gmail.com>
2022-03-30 23:55:53 +0100

    add torrent download limit

--- NyaaTranspiler/entities/DataProcess.py
+++ NyaaTranspiler/entities/DataProcess.py
@@ -1,5 +1,6 @@
 import requests
 import re
+import os
 from collections import OrderedDict
 from bs4 import BeautifulSoup
 class DataProcess(object):
@@ -14,7 +15,7 @@ class DataProcess(object):
         magnet = soup.find('a', 'card-footer-item').get('href').strip()
         return magnet
     
-    def parse_rss_feed(self, url):
+    def parse_rss_feed(self, url, limit=None):
         """"Parse the RSS feed coming from Nyaa.si website
             ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
         Basic usage:
@@ -42,36 +43,31 @@ class DataProcess(object):
 
         # Find all torrent files and magnets
         items = soup.find_all('item')
-        count = 0
-        for item in items:
-            if count == 10:
-                break
-            else:
-                count = count + 1
-                anime = OrderedDict()
-                anime['title'] = item.title.text.strip()
-                anime['torrent_file'] = self.get_torrent_link(item.guid.text.strip())
-                anime['info_link'] = {
-                    "url" : item.guid.text.strip(),
-                    "isPermaLink" : item.guid.get('isPermaLink')
-                }
-                anime['Published_at'] = item.pubdate.text.strip()
-                anime['seeders'] = item.find('nyaa:seeders').text.strip()
-                anime['leechers'] = item.find('nyaa:leechers').text.strip()
-                anime['downloads'] = item.find('nyaa:downloads').text.strip()
-                anime['infoHash'] = item.find('nyaa:infohash').text.strip()
-                anime['category'] ={
-                    'id' : item.find('nyaa:categoryid').text.strip(),
-                    'category__name' : item.find('nyaa:category').text.strip()
-                },
-                anime['file__size'] = item.find('nyaa:size').text.strip()
-                anime['comments'] = item.find('nyaa:comments').text.strip()
-                anime['is__trusted__torrent'] = {
-                    'text' : item.find('nyaa:trusted').text.strip(),
-                    'value': False if item.find('nyaa:trusted').text.strip() == 'No' else True, 
-                },
-                anime['is__remake'] = item.find('nyaa:remake').text.strip()
-                obj['data'].append(anime)
+        for item in (items[:limit] if limit is not None else items):
+            anime = OrderedDict()
+            anime['title'] = item.title.text.strip()
+            anime['torrent_file'] = self.get_torrent_link(item.guid.text.strip())
+            anime['info_link'] = {
+                "url" : item.guid.text.strip(),
+                "isPermaLink" : item.guid.get('isPermaLink')
+            }
+            anime['Published_at'] = item.pubdate.text.strip()
+            anime['seeders'] = item.find('nyaa:seeders').text.strip()
+            anime['leechers'] = item.find('nyaa:leechers').text.strip()
+            anime['downloads'] = item.find('nyaa:downloads').text.strip()
+            anime['infoHash'] = item.find('nyaa:infohash').text.strip()
+            anime['category'] ={
+                'id' : item.find('nyaa:categoryid').text.strip(),
+                'category__name' : item.find('nyaa:category').text.strip()
+            },
+            anime['file__size'] = item.find('nyaa:size').text.strip()
+            anime['comments'] = item.find('nyaa:comments').text.strip()
+            anime['is__trusted__torrent'] = {
+                'text' : item.find('nyaa:trusted').text.strip(),
+                'value': False if item.find('nyaa:trusted').text.strip() == 'No' else True, 
+            },
+            anime['is__remake'] = item.find('nyaa:remake').text.strip()
+            obj['data'].append(anime)
         return obj
 
     def RSS_create_search_query(self, filter_=None, search_string=None, category=None, username=None):
@@ -93,8 +89,31 @@ class DataProcess(object):
             for key, value in q.items():
                 pass
                 query += f"&{key}={value}"
-        
-        return (base_url + query)  
+        return (base_url + query)
+    
+    def get_torrent_files(self, url, limit=None):
+        feed_data = self.RSS_get_latest_feed_data(url, rtype='dict', limit=limit)
+        base_dir = os.path.dirname(__file__)
+        mdir = os.path.join(base_dir, "automated")
+        if os.path.exists(mdir) == False:
+            os.mkdir(mdir)
+            print('Directory created.')
+        else:
+            print('directory exists.')
+        for item in feed_data['data']:  
+            with requests.get(item['torrent_file'], stream=True) as r:
+                r.raise_for_status()
+                invalid_chars = f'<>:"\/|?*'
+                pattern = r'[' + invalid_chars + ']'
+                new_name = re.sub(pattern, ' ', item['title'])
+                with open(os.path.join(mdir, 'log.txt'), 'a', encoding='utf-8') as log:
+                    log.write(f"File saved: {new_name}.torrent \n")
+                with open(os.path.join(mdir, f"{new_name}.torrent"), "wb") as f:
+                    for chunk in r.iter_content():
+                        if chunk:
+                            f.write(chunk)  
+                        
+
 debug = DataProcess()
-# debug.parse_rss_feed()
+
 
--- NyaaTranspiler/entities/NyaaRSS.py
+++ NyaaTranspiler/entities/NyaaRSS.py
@@ -1,14 +1,20 @@
-﻿from DataProcess import DataProcess
+﻿"""
+    TODO:
+        ---Don't overwrite exisiting torrent files/data
+        ---Check query if user has submitted valid input
+        ---Add more debug console data.
+"""
+
+from DataProcess import DataProcess
 import json
 import requests
-import os
 import re
 from json import JSONDecodeError
 import pprint
 import string
 class NyaaRSS(DataProcess):
-    def RSS_get_feed_data(self, url, rtype=None):
-        feed_data = self.parse_rss_feed(url)
+    def RSS_get_latest_feed_data(self, url, rtype=None, limit=None):
+        feed_data = self.parse_rss_feed(url, limit=limit)
         try:
             if rtype == 'json':
                 return json.dumps(feed_data)
@@ -22,46 +28,35 @@ class NyaaRSS(DataProcess):
         except JSONDecodeError:
             raise ('Invalid type, try again. i.e --> rtype="dict"/rtype="json"')
         
-    def RSS_get_torrent_files(self, url):
-        feed_data = self.RSS_get_feed_data(url, rtype='dict')
-        base_dir = os.path.dirname(__file__)
-        mdir = os.path.join(base_dir, "automated")
-        if os.path.exists(mdir) == False:
-            os.mkdir(mdir)
-            print('Directory created.')
-        else:
-            print('directory exists.')
-        for item in feed_data['data']:  
-            with requests.get(item['torrent_file'], stream=True) as r:
-                r.raise_for_status()
-                invalid_chars = f'<>:"\/|?*'
-                pattern = r'[' + invalid_chars + ']'
-                new_name = re.sub(pattern, ' ', item['title'])
-                with open(os.path.join(mdir, 'log.txt'), 'a', encoding='utf-8') as log:
-                    log.write(f"File saved: {new_name}.torrent \n")
-                with open(os.path.join(mdir, f"{new_name}.torrent"), "wb") as f:
-                    for chunk in r.iter_content():
-                        if chunk:
-                            f.write(chunk)
 
     def RSS_query_search_data(self, filter_type=None, query=None, category=None, username=None):
         search_url = self.RSS_create_search_query(filter_=filter_type, 
                                      search_string=query, 
                                      category=category, 
                                      username=username)
-        search_feed_data = self.RSS_get_feed_data(search_url, rtype='dict')
+        search_feed_data = self.RSS_get_latest_feed_data(search_url, rtype='dict')
         return search_feed_data
         
-    def RSS_get_query_search_torrents(self, filter_type=None, query=None, category=None, username=None ):
+    def RSS_get_query_search_torrents(self, 
+                                      filter_type=None, 
+                                      query=None, 
+                                      category=None, 
+                                      username=None, 
+                                      limit=None):
         search_url = self.RSS_create_search_query(filter_=filter_type, 
                                      search_string=query, 
                                      category=category, 
                                      username=username)
-        self.RSS_get_torrent_files(search_url)
+        self.get_torrent_files(search_url)
 
-    def RSS_search_by_username(self, username=None):
+    def RSS_search_data_by_username(self, username=None):
         search_url = self.RSS_create_search_query(username=username)
-        self.RSS_get_torrent_files(search_url)
+        return self.RSS_get_latest_feed_data(search_url)
+        
+    def RSS_search_torrents_by_username(self, username=None):
+        search_url = self.RSS_create_search_query(username=username)
+        self.get_torrent_files(search_url)
+        
         
 rss = NyaaRSS()
 rss.RSS_get_query_search_torrents(query='Digimon adventure')
